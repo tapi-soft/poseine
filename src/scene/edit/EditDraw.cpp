c597cf0a9b3377e5aa465891add0b10cdeacca47
posemiya <posemiya@gmail.com>
2016-02-19 14:17:07 +0900

    シナリオのサムネ表示

--- src/scene/edit/EditDraw.cpp
+++ src/scene/edit/EditDraw.cpp
@@ -24,10 +24,21 @@ void EditDraw::update()
 {
     DrawGraph(0, 0, image_back, TRUE);
 
-    // edit
-    // preview
+    //---- edit
+    //---- preview
     ScenarioDraw::draw(edit_state->getScenarioNum(), 428, 0, 0.665625);
 
-    // timeline
-    // scenario select
+    //---- timeline
+    //---- scenario select
+    int num = edit_state->getScenarioNum();
+    int prev1 = AllScenarioData::getInstance()->getPrev(num);
+    int prev2 = AllScenarioData::getInstance()->getPrev(prev1);
+    int next1 = AllScenarioData::getInstance()->getNext(num);
+    int next2 = AllScenarioData::getInstance()->getNext(next1);
+
+    if (prev2 != 0) { ScenarioDraw::draw(prev2, 500, 625, 0.1); }
+    if (prev1 != 0) { ScenarioDraw::draw(prev1, 650, 625, 0.1); }
+    ScenarioDraw::draw(num, 800, 625, 0.1);
+    if (next1 != 0) { ScenarioDraw::draw(next1, 950, 625, 0.1); }
+    if (next2 != 0) { ScenarioDraw::draw(next2, 1100, 625, 0.1); }
 }
