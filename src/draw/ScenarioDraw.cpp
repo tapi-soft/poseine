2cdefa007ec9891b99bce7739e6954c1a58c62f6
posemiya <posemiya@gmail.com>
2016-03-01 23:35:45 +0900

    編集画面も優先順位通り描画されるように修正

--- src/draw/ScenarioDraw.cpp
+++ src/draw/ScenarioDraw.cpp
@@ -41,21 +41,25 @@ void ScenarioDraw::drawBack(int n, int x, int y, double rate)
 //---------------------------------------------------------------------
 void ScenarioDraw::drawChara(int num, int x, int y, double rate)
 {
-    for (int n = 1; n <= AllScenarioData::getInstance()->getCharaNum(num); n++) {
-        int chara_image = AllScenarioData::getInstance()->getCharaImage(num, n);
-        int chara_pos = AllScenarioData::getInstance()->getCharaPos(num, n);
-        int image = ImageData::getInstance()->getImageChara(chara_image);
+    for (int priority = ScenarioData::CHARA_POS_MAX; priority >= 1; priority--) {
+        for (int n = 1; n <= AllScenarioData::getInstance()->getCharaNum(num); n++) {
+            int chara_image = AllScenarioData::getInstance()->getCharaImage(num, n);
+            int chara_pos = AllScenarioData::getInstance()->getCharaPos(num, n);
+            int image = ImageData::getInstance()->getImageChara(chara_image);
+            
+            if (chara_pos != priority) { continue; }
 
-        switch (chara_pos) {
-        case 1: DrawExtendGraph((int)(x + 240 * rate), (int)(y - 100 * rate), (int)(x + 1040 * rate), (int)(y + 1550 * rate), image, TRUE); break;
-        case 2: DrawExtendGraph((int)(x + 390 * rate), (int)(y + 100 * rate), (int)(x + 890 * rate),  (int)(y + 1100 * rate), image, TRUE); break;
-        case 3: DrawExtendGraph((int)(x + 90 * rate),  (int)(y + 100 * rate), (int)(x + 590 * rate),  (int)(y + 1100 * rate), image, TRUE); break;
-        case 4: DrawExtendGraph((int)(x + 690 * rate), (int)(y + 100 * rate), (int)(x + 1190 * rate), (int)(y + 1100 * rate), image, TRUE); break;
-        case 5: DrawExtendGraph((int)(x + 490 * rate), (int)(y + 200 * rate), (int)(x + 790 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
-        case 6: DrawExtendGraph((int)(x + 90 * rate),  (int)(y + 200 * rate), (int)(x + 390 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
-        case 7: DrawExtendGraph((int)(x + 890 * rate), (int)(y + 200 * rate), (int)(x + 1190 * rate), (int)(y + 800 * rate),  image, TRUE); break;
-        case 8: DrawExtendGraph((int)(x + 290 * rate), (int)(y + 200 * rate), (int)(x + 590 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
-        case 9: DrawExtendGraph((int)(x + 690 * rate), (int)(y + 200 * rate), (int)(x + 990 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
+            switch (chara_pos) {
+            case 1: DrawExtendGraph((int)(x + 240 * rate), (int)(y - 100 * rate), (int)(x + 1040 * rate), (int)(y + 1550 * rate), image, TRUE); break;
+            case 2: DrawExtendGraph((int)(x + 390 * rate), (int)(y + 100 * rate), (int)(x + 890 * rate),  (int)(y + 1100 * rate), image, TRUE); break;
+            case 3: DrawExtendGraph((int)(x + 90 * rate),  (int)(y + 100 * rate), (int)(x + 590 * rate),  (int)(y + 1100 * rate), image, TRUE); break;
+            case 4: DrawExtendGraph((int)(x + 690 * rate), (int)(y + 100 * rate), (int)(x + 1190 * rate), (int)(y + 1100 * rate), image, TRUE); break;
+            case 5: DrawExtendGraph((int)(x + 490 * rate), (int)(y + 200 * rate), (int)(x + 790 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
+            case 6: DrawExtendGraph((int)(x + 90 * rate),  (int)(y + 200 * rate), (int)(x + 390 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
+            case 7: DrawExtendGraph((int)(x + 890 * rate), (int)(y + 200 * rate), (int)(x + 1190 * rate), (int)(y + 800 * rate),  image, TRUE); break;
+            case 8: DrawExtendGraph((int)(x + 290 * rate), (int)(y + 200 * rate), (int)(x + 590 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
+            case 9: DrawExtendGraph((int)(x + 690 * rate), (int)(y + 200 * rate), (int)(x + 990 * rate),  (int)(y + 800 * rate),  image, TRUE); break;
+            }
         }
     }
 }
